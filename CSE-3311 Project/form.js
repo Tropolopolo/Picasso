d33cd3160cd016f686d31cf1bd7123ce8eca5b6f
anmstha123 <61106764+anmstha123@users.noreply.github.com>
2022-03-07 18:19:24 -0600

    3/7/2022-commit

---
+++ CSE-3311 Project/form.js
@@ -0,0 +1,114 @@
+//Youtube video used = https://www.youtube.com/watch?v=RAWHXRTKTHw
+//*************************************************Firebase configuration********************************************************//
+const firebaseConfig = {
+  apiKey: "AIzaSyBganeN22quF9d0vAqJeAgYr9BCQyioKeg",
+  authDomain: "picasso-2f8a6.firebaseapp.com",
+  databaseURL: "https://picasso-2f8a6-default-rtdb.firebaseio.com",
+  projectId: "picasso-2f8a6",
+  storageBucket: "picasso-2f8a6.appspot.com",
+  messagingSenderId: "658469420641",
+  appId: "1:658469420641:web:951806d6a4eae684827420",
+  measurementId: "G-HWY7NTX42Q"
+};
+//reference your database
+firebase.initializeApp(firebaseConfig);
+
+//referencing a database named "Picasso"
+//if it does not exists, it is created
+let database = firebase.database();
+
+database.ref("Picasso").on("value",getData,errData);
+
+//*************************************************Get Data Function*************************************************************//
+//To get data
+function getData(data){
+
+
+  let ProjectName;
+  let ContractorName;
+  let ContractorPN;
+  let ProjectManager;
+  let ProjectManagerPN;
+  let PaintStoreManagerName;
+  let PaintStoreManagerPN;
+  let leadPinterName;
+  let leadPinterPN;
+  let paintStoreName;
+  let paintStoreAddress;
+  let paintStorePN;
+  let projectAddress;
+  //console.log(data.val());
+  let objectData = data.val();
+  let keys = Object.keys(objectData);
+  //console.log(keys);
+
+    let k = localStorage.getItem("value");
+    // ProjectName = objectData[k].ProjectName;
+    // Address = objectData[k].Address;
+    // ProjectManager = objectData[k].ProjectManager;
+    // MatsUsed = objectData[k].MatsUsed;
+    ProjectName           =objectData[k].ProjectName;
+    ContractorName        =objectData[k].ContractorName;
+    ContractorPN          =objectData[k].ContractorPN;
+    ProjectManager        =objectData[k].ProjectManager;
+    ProjectManagerPN      =objectData[k].ProjectManagerPN;
+    PaintStoreManagerName =objectData[k].PaintStoreManagerName;
+    PaintStoreManagerPN   =objectData[k].PaintStoreManagerPN;
+    leadPinterName        =objectData[k].leadPinterName;
+    leadPinterPN          =objectData[k].leadPinterPN;
+    paintStoreName        =objectData[k].paintStoreName;
+    paintStoreAddress     =objectData[k].paintStoreAddress;
+    paintStorePN          =objectData[k].paintStorePN;
+    projectAddress        =objectData[k].projectAddress;
+
+    //Creating a list to put the data in
+    let li = document.createElement("ul");
+    li.className="list";
+    li.id="list";
+    li.innerHTML='<table class="projectTable">' +
+                 '<tr>'+'<th>'+"Project Name: "+'</th>'+'<th>'+ ProjectName +'</th>'+'</tr>'+
+                 '<tr>'+'<th>'+"Contractor Name: " +'</th>'+'<th>'+ContractorName+'</th>' + '</tr>'+
+                 '<tr>'+'<th>'+"Contractor PN: " + '</th>'+'<th>'+ContractorPN +'</th>'+ '</tr>'+
+                 '<tr>'+'<th>'+"Project Manager: "+ '</th>'+'<th>' +ProjectManager+'</th>' + '</tr>'+
+                 '<tr>'+'<th>'+"ProjectManager PN: "+'</th>'+'<th>'+ProjectManagerPN +'</th>'+'</tr>'+
+                 '<tr>'+'<th>'+"PaintStoreManager Name: " +'</th>'+'<th>'+PaintStoreManagerName+'</th>' + '</tr>'+
+                 '<tr>'+'<th>'+"PaintStoreManager PN: " + '</th>'+'<th>'+PaintStoreManagerPN +'</th>'+ '</tr>'+
+                 '<tr>'+'<th>'+"leadPinter Name: "+ '</th>'+'<th>' + leadPinterName+'</th>' + '</tr>'+
+                 '<tr>'+'<th>'+"leadPinter PN: "+ '</th>'+'<th>' + leadPinterPN+'</th>' + '</tr>'+
+                 '<tr>'+'<th>'+"paintStore Name: " + '</th>'+'<th>'+paintStoreName +'</th>'+ '</tr>'+
+                 '<tr>'+'<th>'+"paintStore Address: "+ '</th>'+'<th>' + paintStoreAddress+'</th>' + '</tr>'+
+                 '<tr>'+'<th>'+"paintStore PN: "+ '</th>'+'<th>' + paintStorePN+'</th>' + '</tr>'+
+                 '<tr>'+'<th>'+"project Address: "+ '</th>'+'<th>' + projectAddress+'</th>' + '</tr>'
+                 '</table>';
+
+  //Creating a box
+  //All this is just to put the data in a box in html, style is in css as Box1 and P1
+  //toAdd is a document fragment to put the box in
+  //var toAddBox = document.createDocumentFragment();
+  //creating a div so that we can give the box an id and class
+  let newDiv = document.createElement('div');
+  newDiv.id = "Box";
+  newDiv.className = 'Box2';
+
+
+
+  
+  //creating a div to put the retrived data in
+  let Para = document.createElement('div');
+  Para.className="P1"
+
+  Para.appendChild(li);
+  newDiv.appendChild(Para);
+
+  // newDiv.appendChild(dropDownDiv);
+  //toAddBox.appendChild(newDiv);
+  document.getElementById('body').appendChild(newDiv);
+
+
+  
+}
+//To get data
+function errData(err){
+  console.log("Error");
+  console.log(err);
+}
--- CSE-3311 Project/server.js
+++ CSE-3311 Project/server.js
@@ -39,9 +39,9 @@ function getData(data){
   }
 
   let ProjectName;
-  let Address;
+  let ContractorName;
   let ProjectManager;
-  let MatsUsed;
+  let projectAddress;
   //console.log(data.val());
   let objectData = data.val();
   let keys = Object.keys(objectData);
@@ -51,21 +51,21 @@ function getData(data){
   {
 
     let k = keys[i];
-    ProjectName = objectData[k].ProjectName;
-    Address = objectData[k].Address;
-    ProjectManager = objectData[k].ProjectManager;
-    MatsUsed = objectData[k].MatsUsed;
+    ProjectName           =objectData[k].ProjectName;
+    ContractorName        =objectData[k].ContractorName;
+    ProjectManager        =objectData[k].ProjectManager;
+    projectAddress        =objectData[k].projectAddress;
 
     //Creating a list to put the data in
     let li = document.createElement("ul");
     li.className="list";
     li.id="list";
     li.innerHTML='<table class="projectTable">' +
-                 '<tr>'+'<th>'+"Project Name: "+'</th>'+'<th>'+ ProjectName +'</th>'+'</tr>'+
-                 '<tr>'+'<th>'+"Address: " +'</th>'+'<th>'+Address+'</th>' + '</tr>'+
-                 '<tr>'+'<th>'+"Project Manager: " + '</th>'+'<th>'+ProjectManager +'</th>'+ '</tr>'+
-                 '<tr>'+'<th>'+" Materials Used: "+ '</th>'+'<th>' + MatsUsed+'</th>' + '</tr>'+
-                 '</table>';
+                '<tr>'+'<th>'+"Project Name: "+'</th>'+'<th>'+ ProjectName +'</th>'+'</tr>'+
+                '<tr>'+'<th>'+"Contractor Name: " +'</th>'+'<th>'+ContractorName+'</th>' + '</tr>'+
+                '<tr>'+'<th>'+"Project Manager: "+ '</th>'+'<th>' +ProjectManager+'</th>' + '</tr>'+
+                '<tr>'+'<th>'+"project Address: "+ '</th>'+'<th>' + projectAddress+'</th>' + '</tr>'
+                '</table>';
 
   //Creating a box
   //All this is just to put the data in a box in html, style is in css as Box1 and P1
@@ -74,24 +74,42 @@ function getData(data){
   //creating a div so that we can give the box an id and class
   let newDiv = document.createElement('div');
   newDiv.id = "Box";
-  newDiv.className = 'Box1'; 
+  newDiv.className = 'Box1';
+
+  
 
   let cl = document.createElement("button");
-  cl.className="close";
-  cl.textContent="X";
+  cl.className="close1";
+  cl.textContent="Delete Project";
   cl.addEventListener("click", function(){
-    database.ref('Picasso/' + k).remove();
+    if (confirm("Are you sure you want to delete the project")) {
+      database.ref('Picasso/' + k).remove();
+    } else {
+    }
+    
     
     //window.location.reload();
   });
+
   
   //creating a div to put the retrived data in
   let Para = document.createElement('div');
   Para.className="P1"
   //appending to html
+  let seeMore = document.createElement("button");
+  seeMore.className="seeMore";
+  seeMore.innerHTML="<a href='http://127.0.0.1:5500/form.html' style='text-decoration:none;' alt='Broken Link' target='_blank'>see More</a>";
+  seeMore.onclick=function(){
+    localStorage.setItem("value",k);
+    console.log(k);
+  };
+  
+  
   Para.appendChild(li);
   newDiv.appendChild(Para);
   newDiv.appendChild(cl);
+  newDiv.appendChild(seeMore);
+  // newDiv.appendChild(dropDownDiv);
   //toAddBox.appendChild(newDiv);
   document.getElementById('body').appendChild(newDiv);
 
@@ -112,7 +130,6 @@ function getData(data){
 function errData(err){
   console.log("Error");
   console.log(err);
-
 }
 
 //***********************************************Function to submit form*********************************************************//
@@ -124,11 +141,20 @@ function submitForm(e){
   //getting all the data from the form
   let data = {
     ProjectName:getElementVal("ProjectName"),
-    Address:getElementVal("Address"),
+    ContractorName:getElementVal("ContractorName"),
+    ContractorPN:getElementVal("ContractorPN"),
     ProjectManager:getElementVal("ProjectManager"),
-    MatsUsed:getElementVal("MatsUsed")
+    ProjectManagerPN:getElementVal("ProjectManagerPN"),
+    PaintStoreManagerName:getElementVal("PaintStoreManagerName"),
+    PaintStoreManagerPN:getElementVal("PaintStoreManagerPN"),
+    leadPinterName:getElementVal("leadPinterName"),
+    leadPinterPN:getElementVal("leadPinterPN"),
+    paintStoreName:getElementVal("paintStoreName"),
+    paintStoreAddress:getElementVal("paintStoreAddress"),
+    paintStorePN:getElementVal("paintStorePN"),
+    projectAddress:getElementVal("projectAddress")
   }
-  let formDB = database.ref("/Picasso/");
+  let formDB = database.ref("Picasso/");
   //pushing the data to the databases
   formDB.push(data);
 
@@ -137,6 +163,7 @@ function submitForm(e){
 
   //resetting the form
   document.getElementById("Form").reset();
+  window.location.reload();
   
 }
 
@@ -153,5 +180,80 @@ function showAlert(){
 const getElementVal = (id)=>{
   return document.getElementById(id).value;
 }
+
+
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form ...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  // ... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  // ... and run a function that displays the correct step indicator:
+  fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form... :
+  if (currentTab >= x.length) {
+    //...the form gets submitted:
+    document.getElementById("regForm").submit();
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+}
+
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x, y, i, valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == "") {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false:
+      valid = false;
+    }
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; // return the valid status
+}
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class to the current step:
+  x[n].className += " active";
+}
+
 
 
